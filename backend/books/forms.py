103a3003ab6bff1fbe586f08ba6993d84d35df52
inu9431 <inu9431@naver.com>
2025-06-24 16:33:21 +0900

    검색기능 삭제기능 수정

--- backend/books/forms.py
+++ backend/books/forms.py
@@ -2,3 +2,7 @@ from django import forms
 
 class BookSearchForm(forms.Form):
     title = forms.CharField(label='책 제목', max_length=100)
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        return title.strip()
