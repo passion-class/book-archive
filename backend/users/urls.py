7af2f965956b12c7985fdc4b71871ffca2499c51
KIM SHINEDAY <kimshineday99@gmail.com>
2025-07-07 19:17:59 +0900

    💄 책 검색&등록 페이지 nav 수정
    마이페이지 & 책 목록 페이지 링크

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -8,7 +8,5 @@ app_name = 'users'
 
 urlpatterns = [
     path("signup/", views.signup_view, name="signup"),
-    # path("login/", views.login_view, name="login"),
-    # path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("info/", views.my_info_view, name="info"),
     ]
